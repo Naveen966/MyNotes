b7bdced028b0471e3aa35142d68a8deda6489a64
Naveen966 <naveenkumar85600@gmail.com>
2023-01-14 17:30:31 +0530

    First Commit

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const connection = require("./db");
 const path = require("path");
-const { dirname } = require("path");
 
 connection();
 
